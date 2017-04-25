1de1b3525f8a1c8bc81ceab02eabe2ef287450a4
lucapette <lucapette@gmail.com>
2017-04-25 11:46:33 +0200

    Introduce version

--- cmd/fakedata.go
+++ cmd/fakedata.go
@@ -11,6 +11,8 @@ import (
 	flag "github.com/spf13/pflag"
 )
 
+var version = "master"
+
 var usage = `
   Usage: fakedata [option ...] [field ...]
 
@@ -19,14 +21,21 @@ var usage = `
     --limit n       limits rows up to n [default: 10]
     --help          shows help information
     --format f      generates rows in f format [options: csv|tab, default: " "]
+    --version       shows version information
 `
 
 var generatorsFlag = flag.Bool("generators", false, "lists available generators")
 var limitFlag = flag.Int("limit", 10, "limits rows up to n")
 var helpFlag = flag.Bool("help", false, "shows help information")
 var formatFlag = flag.String("format", "", "generators rows in f format")
+var versionFlag = flag.Bool("version", false, "shows version information")
 
 func main() {
+	if *versionFlag {
+		fmt.Println(version)
+		os.Exit(0)
+	}
+
 	if *helpFlag {
 		fmt.Print(usage)
 		os.Exit(0)
