ceff303dd551c289de110daad69e060c29cdda88
lucapette <lucapette@gmail.com>
2017-04-25 23:23:49 +0200

    Increase coverage a bit

---
+++ pkg/fakedata/formatter_test.go
@@ -0,0 +1,48 @@
+package fakedata_test
+
+import (
+	"testing"
+
+	"github.com/lucapette/fakedata/pkg/fakedata"
+)
+
+var columns = fakedata.Columns{{Name: "name", Key: "name"}, {Name: "domain", Key: "domain"}}
+var values = []string{"Grace Hopper", "example.com"}
+
+func TestSeparatorFormatter(t *testing.T) {
+	tests := []struct {
+		name string
+		sep  string
+		want string
+	}{
+		{"default", " ", "Grace Hopper example.com"},
+		{"csv", ",", "Grace Hopper,example.com"},
+		{"tab", "\t", "Grace Hopper\texample.com"},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			f := &fakedata.SeparatorFormatter{Separator: tt.sep}
+			if got := f.Format(columns, values); got != tt.want {
+				t.Errorf("SeparatorFormatter.Format() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestSQLFormatter(t *testing.T) {
+	tests := []struct {
+		name  string
+		table string
+		want  string
+	}{
+		{"table answer", "ANSWER", "INSERT INTO ANSWER (name,domain) VALUES ('Grace Hopper','example.com');"},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			f := &fakedata.SQLFormatter{Table: tt.table}
+			if got := f.Format(columns, values); got != tt.want {
+				t.Errorf("SQLFormatter.Format() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
