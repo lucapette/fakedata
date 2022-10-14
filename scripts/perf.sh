0fece40f60ee64ab5f2ede92ac192318ab3f66aa
lucapette <ciao@lucapette.me>
2022-10-14 16:00:38 +0200

    Add a sqlite3 for datasette

--- scripts/perf.sh
+++ scripts/perf.sh
@@ -3,7 +3,7 @@
 set -euo pipefail
 IFS=$'\n\t'
 
-ROWS=100000000
+ROWS=${ROWS:-100000000}
 
 echo "tick,rows_done" > template.perf
 
@@ -17,4 +17,9 @@ fakedata noun -l ${ROWS} | pv -b -l -a -t -n > /dev/null 2>> generator.perf
 
 sed -i -e 's/ /,/g' generator.perf
 
+cat generator.perf | sqlite-utils insert -d --csv perf.db generator -
+cat template.perf | sqlite-utils insert -d --csv perf.db template -
+
 rm template.perf-e generator.perf-e
+
+cat queries.csv | sqlite-utils insert --csv perf.db saved_queries  -
